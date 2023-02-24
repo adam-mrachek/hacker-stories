a588121dfec0a1a181a4f921b61ab0512177d75a
adam <amrachek@gmail.com>
2023-02-24 12:35:31 -0700

    Finish imperative React lesson

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ function App() {
       <InputWithLabel
         id="search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>
@@ -61,12 +62,33 @@ function App() {
   );
 }
 
-function InputWithLabel({ id, value, type = "text", onInputChange, children }) {
+function InputWithLabel({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
   return (
     <>
       <label htmlFor={id}>{children}</label>
       &nbsp;
-      <input id={id} type={type} value={value} onChange={onInputChange} />
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
     </>
   );
 }
